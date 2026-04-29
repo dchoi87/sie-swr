28f910791c203202b39f7744dd052710f7a799ed
Daniel Choi <dchoi87@gmail.com>
2026-04-29 11:45:39 -0700

    Layer Component Update

--- src/components/molecules/Layer/Layer.tsx
+++ src/components/molecules/Layer/Layer.tsx
@@ -8,14 +8,21 @@ export interface LayerProps {
 
 const Layer = ({ title }: LayerProps) => (
   <div className={styles.container}>
-    <div className={styles.title}>{title}</div>
-    <div className={styles.description}>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua.
+    <div className={styles.copy}>
+      <div className={styles.title}>{title}</div>
+      <div className={styles.description}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </div>
     </div>
     <div className={styles.cta}>
       <Button label="Lorem Ipsum" />
       <Button label="Lorem Ipsum" type="secondary" />
+    </div>
+    <div className={styles.dots}>
+      {[...Array(5)].map((_el, i) => {
+        return <div key={`dot-${i}`} className={styles.dot}></div>;
+      })}
     </div>
   </div>
 );
