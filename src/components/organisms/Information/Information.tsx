e487ed9197e6713eb5c422aebc3db78d295cc77f
Daniel Choi <dchoi87@gmail.com>
2026-05-08 17:01:11 -0700

    Help Animation

--- src/components/organisms/Information/Information.tsx
+++ src/components/organisms/Information/Information.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 import { Dialogue, Help } from "@/components/molecules";
 
@@ -7,25 +7,40 @@ import styles from "./Information.module.scss";
 export interface InformationProps {}
 
 const Information = ({}: InformationProps) => {
-  const [showHelp, setHelp] = useState(true);
+  const [showHelp, setShowHelp] = useState(false);
+  const timeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
 
-  const handleMouseEnter = () => {};
+  const handleHelpEnter = () => {
+    timeoutRef.current = setTimeout(() => {
+      setShowHelp(true);
+    }, 2200);
+  };
+
+  const handleInformationLeave = () => {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+      timeoutRef.current = null;
+    }
+
+    setShowHelp(false);
+  };
 
   return (
-    <div className={styles.container}>
+    <div
+      className={styles.container}
+      data-help-open={showHelp}
+      onMouseLeave={handleInformationLeave}
+    >
       <Dialogue
         title="Need Help?"
         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
         navigation="chevron"
-        help={true}
-        onMouseEnter={() => {
-          // setHelp(true);
-        }}
-        onMouseLeave={() => {
-          // setHelp(false);
-        }}
+        help
+        onMouseEnter={handleHelpEnter}
       />
-      {showHelp && <Help />}
+      <div className={styles.helpPanel} aria-hidden={!showHelp}>
+        <Help />
+      </div>
     </div>
   );
 };
