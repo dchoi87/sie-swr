002c99fa8eca4fc93407264a2223f522338f6c23
Daniel Choi <dchoi87@gmail.com>
2026-05-10 15:43:42 -0700

    CSS Compiling Fix

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,8 +1,13 @@
 import type { StorybookConfig } from "@storybook/react-vite";
+import { mergeConfig } from "vite";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: ["storybook-addon-tag-badges"],
   framework: "@storybook/react-vite",
+  viteFinal: async (config) =>
+    mergeConfig(config, {
+      build: { cssMinify: "esbuild" },
+    }),
 };
 export default config;
