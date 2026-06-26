d6aa5b06efa1a02ffa9a1a2bb32990fe9a996ed6
Daniel Choi <dchoi87@gmail.com>
2026-06-26 15:44:37 -0700

    Update Barrel Import

--- src/components/organisms/index.ts
+++ src/components/organisms/index.ts
@@ -2,4 +2,5 @@ export { default as Calibration } from "./Calibration";
 export { default as Completion } from "./Completion";
 export { default as Introduction } from "./Introduction";
 export { default as Medication } from "./Medication";
+export { default as PatientHistory } from "./PatientHistory";
 export { default as VisualAcuity } from "./VisualAcuity";
