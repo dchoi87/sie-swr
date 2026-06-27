f9e753464e9e898f2f759c24bc52ab7d30574a36
Daniel Choi <dchoi87@gmail.com>
2026-06-27 11:28:19 -0700

    Progress Bar

--- src/components/templates/Flow/Flow.tsx
+++ src/components/templates/Flow/Flow.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import classNames from "classnames";
 
 import { FlowContext } from "@/hooks/useFlow";
 
@@ -11,24 +12,44 @@ import {
   Completion,
 } from "@/components/organisms";
 
-const steps = [
-  Calibration,
-  Introduction,
-  VisualAcuity,
-  Medication,
-  PatientHistory,
-  Completion,
+import styles from "./Flow.module.scss";
+
+type FlowStep = {
+  Component: React.ComponentType;
+  hideDots?: boolean;
+};
+
+const steps: FlowStep[] = [
+  { Component: Calibration, hideDots: true },
+  { Component: Introduction },
+  { Component: VisualAcuity, hideDots: true },
+  { Component: Medication },
+  { Component: PatientHistory },
+  { Component: Completion },
 ];
 
 const Flow = () => {
   const [stepIndex, setStepIndex] = useState(0);
   const next = () =>
     setStepIndex((index) => Math.min(index + 1, steps.length - 1));
-  const Step = steps[stepIndex];
+  const { Component, hideDots } = steps[stepIndex];
 
   return (
     <FlowContext.Provider value={{ next }}>
-      <Step />
+      {!hideDots && (
+        <div className={styles.dots}>
+          {steps.map((_, index) => (
+            <div
+              key={`flow-dot-${index}`}
+              className={classNames(
+                styles.dot,
+                index === stepIndex && styles.selected,
+              )}
+            />
+          ))}
+        </div>
+      )}
+      <Component />
     </FlowContext.Provider>
   );
 };
