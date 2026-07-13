833da3ef6723432bd253d1cf1653b460001d201e
Daniel Choi <dchoi87@gmail.com>
2026-07-13 16:57:07 -0700

    Completion Screen

--- src/components/organisms/Completion/Completion.tsx
+++ src/components/organisms/Completion/Completion.tsx
@@ -1,18 +1,19 @@
-import { Dialogue, ContentBlock } from "@/components/molecules";
+import { Header } from "@/components/molecules";
 
 import styles from "./Completion.module.scss";
 
 export interface CompletionProps {}
 
 const Completion = ({}: CompletionProps) => (
-  <Dialogue hideHelp={[0]}>
-    <div className={styles.container}>
-      <ContentBlock
-        title="You're All Set!"
-        copy="Please return the device to the technician. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt"
-      />
+  <div className={styles.container}>
+    <div className={styles.copy}>
+      <Header title="You're All Set!" alignment="center" />
+      <div className={styles.directions}>
+        Please return the device to the technician. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        eiusmod tempor incididunt
+      </div>
     </div>
-  </Dialogue>
+  </div>
 );
 
 export default Completion;
